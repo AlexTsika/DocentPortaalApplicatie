831194604979d88d16f9c0be5d3e8b4372535c72
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-05-26 15:54:58 +0200

    corrected teacher model

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -8,11 +8,14 @@ use App\Models\Location;
 use Spatie\Searchable\Searchable;
 use Spatie\Searchable\SearchResult;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Teacher extends Model
+class Teacher extends Model implements Searchable
 {
     use HasFactory;
+
+    public function getSearchResult(): SearchResult
+    {
+        $url = route('teachers.show', $this->id);
 
         return new SearchResult(
             $this,
@@ -43,20 +46,6 @@ class Teacher extends Model
         'approved',
         'location_id',
         'category_id',
-    ];
-}
-        return new SearchResult(
-            $this,
-            $this->lastname,
-            $url
-        );
-    }
-
-    public function scopeSearch($query, $term)
-    {
-        return $query->where('lastname', 'LIKE', '%' . $term . '%');
-    }
-}
         'codecity',
         'streetnr',
     ];
